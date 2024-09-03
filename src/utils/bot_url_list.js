f34e5d44357625d2091de9f8d4c8a4b8552f4f8f
Tarigan0700 <tarigan071100@gmail.com>
2024-09-04 01:35:57 +0700

    Menambahkan list bot query getter

--- src/utils/bot_url_list.js
+++ src/utils/bot_url_list.js
@@ -9,6 +9,26 @@ export const botUrlList = [
     bot: "catsgang_bot",
     url: "https://cats-backend-cxblew-prod.up.railway.app",  
   },
+  //Agent301
+  {
+    bot: "Agent301Bot",
+    url: "https://api.agent301.org/completeTask",
+  },
+  //Fintopio
+  {
+    bot: "fintopiobot",
+    url: "https://fintopio-tg.fintopio.com/api",
+  },
+  //HamsterKombat
+  {
+    bot: "hamster_kombat_bot",
+    url: "https://api.hamsterkombat.io/",
+  },
+  //LostDogs
+  {
+    bot: "lost_dogs_bot",
+    url: "https://api.getgems.io/graphql?operationName=getHomePage&variables=%7B%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%22d89d3ccd8d9fd69d37d181e2e8303ee78b80e6a26e4500c42e6d9f695257f9be%22%7D%7D",
+  },
   //#ISI DENGAN DATA BOT YANG INGING DITAMBAHKAN
   // {
   //   bot: "",
