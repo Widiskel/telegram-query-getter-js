7bb4d6fe219394ea63e7585c76640c236a35c9e6
Tarigan0700 <tarigan071100@gmail.com>
2024-09-05 04:59:43 +0700

    Menambahkan list bot query getter

--- src/utils/bot_url_list.js
+++ src/utils/bot_url_list.js
@@ -29,6 +29,11 @@ export const botUrlList = [
     bot: "lost_dogs_bot",
     url: "https://api.getgems.io/graphql?operationName=getHomePage&variables=%7B%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%22d89d3ccd8d9fd69d37d181e2e8303ee78b80e6a26e4500c42e6d9f695257f9be%22%7D%7D",
   },
+  //GOATS
+  {
+    bot : "realgoats_bot",
+    url : "https://dev-api.goatsbot.xyz",
+  },
   //#ISI DENGAN DATA BOT YANG INGING DITAMBAHKAN
   // {
   //   bot: "",
